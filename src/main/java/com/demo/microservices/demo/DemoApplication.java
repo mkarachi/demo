3f84cd4bad16abaeb8ccc952c0340cb9745561b1
Mufaddal Karachiwala <mkarachi@Mufaddals-MacBook-Pro.local>
2020-05-25 03:23:27 -0700

    initial commit

---
+++ src/main/java/com/demo/microservices/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.demo.microservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping(value="/test")
+	public String test() {
+		return "Testing 123";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
